1284b5964ecd1bd3a00d454cf0ec6974d8521d3e
horangijk <iamjooyoung@gmail.com>
2019-04-27 14:48:07 -0400

    Added form and manageTottenham and attached rreact to redux

---
+++ src/components/Form.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type='text'></input>
+                    <input type='submit'></input>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Form
